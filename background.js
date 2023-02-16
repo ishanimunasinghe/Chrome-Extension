0f8f9ce9202f835feb7aa5b32c97ec81675c10dd
Ishani Munasinghe <icmunasi@uwaterloo.ca>
2023-02-15 23:22:06 -0500

    added local storage and data object

--- background.js
+++ background.js
@@ -1,4 +1,30 @@
-// Test if chrome extension is working 
-// alert("chrome extension works");
+chrome.runtime.onInstalled.addListener(details => {
+    console.log("oninstalled reason: ", details.reason)
+}
 
-chrome.runtime.onMessage.addListener(data )
+)
+
+chrome.runtime.onMessage.addListener(data => {
+    const { event, prefs } = data 
+    switch (event){
+        case 'onStop':
+            handleOnStop();
+            break;
+        case 'onStart':
+            handleOnStart(prefs);
+            break;
+        default:
+            break;
+    }
+})
+
+const handleOnStop = () => {
+    console.log("On stop in background")
+}
+
+const handleOnStart = (prefs) => {
+    console.log("On start in background")
+    console.log("prefs recieved:", prefs)
+
+    chrome.storage.local.set(prefs)
+}
--- popup/popup.js
+++ popup/popup.js
@@ -8,15 +8,41 @@ const endLocationElement = document.getElementById("endLocation")
 const startButton = document.getElementById("startButton")
 const cancelButton = document.getElementById("cancelButton")
 
-startButton.onclick = function () {
-    if (timeElement.value){
-        console.log("Time ", timeElement.value)
-    } else {
-        console.log("Time is invald")
+startButton.onclick = () => {
+    const prefs = {
+        departOrArrive: departOrArriveElement.value,
+        timeID: timeElement.value,
+        startLocation: startLocationElement.value,
+        endLocation: endLocationElement.value
+        
     }
+    chrome.runtime.sendMessage({ event: 'onStart', prefs})
+    
     
 }
 
-cancelButton.onclick = function () {
-    console.log("Start Location ", startLocationElement.value)
+cancelButton.onclick = () => {
+    chrome.runtime.sendMessage({ event: 'onStop'})
 }
+
+chrome.storage.local.get(["departOrArrive", "timeID", "startLocation", "endLocation"], (result) => {
+    const { departOrArrive, timeID, startLocation, endLocation } = result;
+
+    if (departOrArrive) {
+        departOrArriveElement.value = departOrArrive
+    }
+
+    if (timeID) {
+        timeElement.value = timeID
+    }
+
+    if (startLocation) {
+        startLocationElement.value = startLocation
+    }
+
+    if (endLocation) {
+        endLocationElement.value = endLocation
+    }
+
+
+})
